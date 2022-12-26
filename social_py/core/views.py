bfa6da8521bb397edab92372ac9c9ad4b7d39af0
dvebino <dhuang2495@gmail.com>
2022-12-26 12:11:47 -0500

    signup POST request created

--- social_py/core/views.py
+++ social_py/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages 
 from django.http import HttpResponse
 # Create your views here.
 
@@ -10,7 +12,21 @@ def signup(request):
     if request.method == "POST":
         username = request.POST['username'];
         password = request.POST['password'];
-        username = request.POST['username'];
         password2 = request.POST['password2'];
+        email = request.POST['email'];
+
+        if password == password2:
+            if User.objects.filter(email = email).exists():
+                messages.info(request, 'Email is taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password);
+                user.save()
+        else:
+            messages.info(request, 'Password does not match')
+            return redirect('signup')
     else:
         return render(request, 'signup.html')
