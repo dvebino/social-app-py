5d3215a97d47bf9f38f14fc1f21d5bf0a0ad50fd
dvebino <dhuang2495@gmail.com>
2023-01-02 16:39:16 -0500

    follower posts showing

--- social_py/core/views.py
+++ social_py/core/views.py
@@ -4,12 +4,27 @@ from django.contrib import messages
 from django.http import HttpResponse
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
+from itertools import chain
 # Create your views here.
 
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username = request.user.username)
     user_profile = Profile.objects.get(user = user_object)
+    
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
 
     feed_posts = Post.objects.all()
 
