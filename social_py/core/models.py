16dc354c4a8dc18020189bf91ad97db82edb7973
dvebino <dhuang2495@gmail.com>
2023-01-02 13:25:29 -0500

    follow/unfollow function working on back-end

--- social_py/core/models.py
+++ social_py/core/models.py
@@ -8,7 +8,7 @@ from datetime import datetime
 User = get_user_model()
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,  on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
