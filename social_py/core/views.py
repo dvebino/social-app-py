7be7755ccd302dbe8d7569d4abc715db4d05d10a
dvebino <dhuang2495@gmail.com>
2023-01-02 13:13:29 -0500

    updated profile forms to views

--- social_py/core/views.py
+++ social_py/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages 
 from django.http import HttpResponse
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -52,7 +52,20 @@ def profile(request,pk):
  
 @login_required(login_url='signin')
 def follow(request):
-    pass
+    if request.method == "POST":
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower,user=user)
+            delete_follower.delete()
+            return redirect('/profile/' + user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower,user = user)
+            new_follower.save()
+            return redirect('/profile/' + user)
+    else:
+        return redirect('/')
 
 @login_required(login_url='signin')
 def settings(request):
